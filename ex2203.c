080ead013b3082c6498ce7565dff85d294793c95
KenjiHarada100 <24eo0118@jec.ac.jp>
2024-12-25 11:05:26 +0900

    test

---
+++ ex2203.c
@@ -0,0 +1,35 @@
+/*
+24EO0118 原田健司
+*/
+
+#include <stdio.h>
+#define SIZE 101
+
+char *str_cut_print(char str);
+
+int main(void){
+
+	char *str;
+	puts("文字列を入力＞"); scanf("%s", &str);
+
+
+
+	return 0;
+}
+
+char *str_cut_print(char *str){
+
+int len;
+int i;
+len = 0;
+i = 0;
+while(str[i] != '\0')
+{
+	len++
+	i++;
+}
+
+while(len != 0)
+
+
+}
